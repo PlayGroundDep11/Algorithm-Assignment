abd48d70daf1187478fb36b6be1638ee9213f989
Sudam Shaluka <rkshaluka@gmail.com>
2023-08-14 01:05:34 +0530

    Algo10 complete

--- Algo10.java
+++ Algo10.java
@@ -7,26 +7,23 @@ public class Algo10 {
     public static void main(String[] args) {
         String numberString ;
         do {
-            System.out.print("Enter your number (+94XX XXXXXXX or 0XX-XXXXXXX) :" );
+            System.out.print("Enter your number (+94 XX XXXXXXX or 0XX-XXXXXXX) :" );
             numberString = scanner.nextLine() ;
+            if(numberString.strip().isBlank()) System.out.println("Telephone number can't be empty");
         } while (numberString.strip().isBlank());
         char[] charArray = numberString.toCharArray();
-        check :
-        {
-        if(numberString.strip().length()!=11 | numberString.strip().length() !=13) break check;
-        if( charArray[charArray.length-8] != ' ' | charArray[charArray.length-8] != '-' ) break check;
-        if( numberString.startsWith("0") | numberString.startsWith("+94") )// break check;
 
+        if (numberString.length() == 11) {
+            if(numberString.startsWith("0") && numberString.charAt(3)==' '){
+                System.out.println("Number is validated");
+            }
+        }else if( numberString.length() == 14){
+            if( numberString.startsWith ("+94") && numberString.charAt(3)==' ' && numberString.charAt(6)==' ' ){
+                System.out.println("Number is validated");
+            }
 
-
-        for(int i = charArray.length-1  ; i >= 0 ; i--){
-            if(i==charArray.length-8)  continue ;
-            if(!Character.isDigit(charArray[i])){
-                break check;
-            } 
         }
-        System.out.println("Number is validated");
-        }
+        else System.out.println("Invalid phone number");
 
     }
 }
